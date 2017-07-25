69a940ad324d2da15194778b96f6d27f8ce2590e
radiyyadwi <radiyyasaputra@gmail.com>
2017-07-25 22:29:24 +0700

    cek aja

--- routes/postQuestRoutes.js
+++ routes/postQuestRoutes.js
@@ -9,22 +9,37 @@ postQuestRouter.route('/createPost')
     res.send("Send Form to user");
   });
 
-postQuestRouter.route('/createPost/submit')
-  .post(function (req, res){
-    let url = 'mongodb://localhost:27017/impuls';
-    console.log(req.body);
-    mongoClient.connect(url, function (err, db) {
-      let collection = db.collection('question');
-      var qtitle =  _.get(req.body, ['title'], 'Isian Kosong');
-      var qbody = req.body.questionbody;
-      const data = {'id' : 1, 'title' : qtitle, 'text' : qbody, 'answer_ids' : [], 'subject_ids' : [], 'chapter_ids' : [] };
-        collection.insertOne(data, function (err, result) {
-          res.send("question submitted");
+  postQuestRouter.route('/createPost/submit')
+    .post(upload.single(),function (req, res){
+      let url = 'mongodb://localhost:27017/impuls';
+      mongoClient.connect(url, function (err, db) {
+        let collection = db.collection('question');
+        var qtitle =  _.get(req.body, ['title'], 'Isian Kosong');
+        var qbody = req.body.questionbody;
 
-          db.close();
+        //image uploader
+        var tempPath = req.files.image.path;
+        console.log(tempPath);
+        var targetPath = path.resolve('./uploads/image.png');
+        if (path.extname(req.files.image.name).toLowerCase() === '.png') {
+          fs.rename(tempPath, targetPath, function(err) {
+            if (err) throw err;
+            console.log("Upload file completed!");
         });
-      });
-  });
+        } else {
+          fs.unlink(tempPath, function () {
+            if (err) throw err;
+            console.error("Only .png files are allowed!");
+          });
+        }
+        const data = {'id' : 1, 'title' : qtitle, 'text' : qbody,'image' : tempPath ,'answer_ids' : [], 'subject_ids' : [], 'chapter_ids' : [] };
+          collection.insertOne(data, function (err, result) {
+            res.send("question submitted");
+
+            db.close();
+          });
+        });
+    });
 
 
 module.exports = postQuestRouter;
