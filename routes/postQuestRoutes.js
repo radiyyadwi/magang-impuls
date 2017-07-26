498ce290727acb42ca758f334cc4eb8686d1796a
radiyyadwi <radiyyasaputra@gmail.com>
2017-07-26 15:27:56 +0700

    add imageuploader

--- routes/postQuestRoutes.js
+++ routes/postQuestRoutes.js
@@ -3,7 +3,9 @@ var express = require('express');
 var postQuestRouter = express.Router();
 var mongoClient = require('mongodb').MongoClient;
 var multer = require('multer');
-var upload = multer({ dest: 'uploads/' });
+var path = require('path');
+var async = require('async');
+var upload = multer({ dest: path.join(__dirname, '../uploads')});
 
 
 postQuestRouter.route('/createPost')
@@ -11,37 +13,36 @@ postQuestRouter.route('/createPost')
     res.send("Send Form to user");
   });
 
-  postQuestRouter.route('/createPost/submit')
-    .post(upload.single(),function (req, res){
-      let url = 'mongodb://localhost:27017/impuls';
-      mongoClient.connect(url, function (err, db) {
-        let collection = db.collection('question');
-        var qtitle =  _.get(req.body, ['title'], 'Isian Kosong');
-        var qbody = req.body.questionbody;
-
-        //image uploader
-        var tempPath = req.files.image.path;
-        console.log(tempPath);
-        var targetPath = path.resolve('./uploads/image.png');
-        if (path.extname(req.files.image.name).toLowerCase() === '.png') {
-          fs.rename(tempPath, targetPath, function(err) {
-            if (err) throw err;
-            console.log("Upload file completed!");
-        });
-        } else {
-          fs.unlink(tempPath, function () {
-            if (err) throw err;
-            console.error("Only .png files are allowed!");
+postQuestRouter.route('/createPost/submit').post(upload.single('image'),function (req, res){
+      const dict = {};
+      async.waterfall([
+        //connect database
+        (flowCallback)=> {
+          let url = 'mongodb://localhost:27017/impuls';
+          mongoClient.connect(url, function (err, db) {
+            if (err) return flowCallback (err);
+            dict.db = db;
+            return flowCallback();
           });
+        },
+        (flowCallback)=> {
+          var question = dict.db.collection('question');
+          var qtitle = _.get(req, ['body', 'title'], null);
+          var qbody = _.get(req, ['body', 'questionbody'], null);
+          var targetPath = _.get(req,['file','path'], null)
+          const data = {'id' : 1, 'title' : qtitle, 'text' : qbody,'image' : targetPath ,'answer_ids' : [], 'subject_ids' : [], 'chapter_ids' : [] };
+          question.insertOne(data, function (err, result) {
+          res.send({"status" : "question submitted",
+                    "judul" : req.body.title,
+                    "image" : req.body.image
+                  });
+          console.log(req.file);
+          console.log(req.body);
+          dict.db.close();
+          })
         }
-        const data = {'id' : 1, 'title' : qtitle, 'text' : qbody,'image' : targetPath ,'answer_ids' : [], 'subject_ids' : [], 'chapter_ids' : [] };
-          collection.insertOne(data, function (err, result) {
-            res.send("question submitted");
+      ])
 
-            db.close();
-          });
-        });
-    });
-
+  });
 
 module.exports = postQuestRouter;
